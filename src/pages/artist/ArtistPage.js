c989384e979faf3b9101c923f58541aa23d6a3a3
Pratham <prathammaharjan1939@gmail.com>
2023-07-25 16:27:31 +0545

    fix artist page

--- src/pages/artist/ArtistPage.js
+++ src/pages/artist/ArtistPage.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { ArtistList } from "./ArtistList";
 import { useForm } from "react-hook-form";
-import {  Heading1 } from "../../components/Heading";
+import { Heading1 } from "../../components/Heading";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllArtists } from "../../redux-store/artistSlice";
 import { Loader } from "../../components/LoaderWrapper";
@@ -10,8 +10,8 @@ const ArtistPage = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        document.title = 'Featured Artist'; 
-      }, []);
+        document.title = "Featured Artist";
+    }, []);
 
     const { artist } = useSelector((state) => state);
 
@@ -26,27 +26,17 @@ const ArtistPage = () => {
     } = useForm();
 
     const getArtists = async () => {
-        
-
-        dispatch(
-            fetchAllArtists({ page: "", limit: "" })
-        );
+        dispatch(fetchAllArtists({ page: "", limit: "" }));
     };
 
     useEffect(() => {
         getArtists();
     }, []);
 
-
     return (
         <div className="max-w-[1800px] m-auto px-[5%] sm:px-[10%]">
-            <div
-                className="md:my-7 my-3"
-                
-            >
+            <div className="md:my-7 my-3">
                 <Heading1 color="black">Artists</Heading1>
-              
-            
             </div>
 
             <div className="flex flex-col justify-center max-w-[1440px] m-auto mt-3 md:mt-6 sm:mt-0">
@@ -54,16 +44,11 @@ const ArtistPage = () => {
                 {fetchStatus !== "success" ? (
                     <Loader />
                 ) : data.artist.length > 0 ? (
-                    <>
-                        <ArtistList artists={data.artist} />
-                        <ArtistList artists={data.artist} />
-                        <ArtistList artists={data.artist} />
-                        <ArtistList artists={data.artist} />
-                    </>
+                    <ArtistList artists={data.artist} />
                 ) : (
                     <div className=" h-[50vh] flex items-center justify-center">
-                    <Heading1> Artist Not Found.</Heading1>
-                </div>
+                        <Heading1> Artist Not Found.</Heading1>
+                    </div>
                 )}
             </div>
         </div>
